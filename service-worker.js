6d03fd38d7cd26ad2fb6c31d7a7a4a4666578ae8
seol6608-commits <seol6608@gmail.com>
2026-09-14 10:32:53 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'study-log-v4';
+const CACHE_NAME = 'study-log-v5';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json?v=4',
+  './manifest.json?v=5',
   './icon-192.png',
   './icon-512.png'
 ];
